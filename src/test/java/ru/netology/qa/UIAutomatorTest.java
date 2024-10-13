03b486f766bf40057c1ad0e7ba265ba1152dc9cf
Olga Korol <oggi.fine@gmail.com>
2024-10-13 18:41:57 +0300

    initial commit

--- src/test/java/ru/netology/qa/UIAutomatorTest.java
+++ src/test/java/ru/netology/qa/UIAutomatorTest.java
@@ -1,8 +1,12 @@
 package ru.netology.qa;
 
-import io.appium.java_client.AppiumDriver;
+
+import io.appium.java_client.android.AndroidDriver;
+
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+
 import ru.netology.qa.screens.MainScreen;
 
 import java.net.MalformedURLException;
@@ -10,41 +14,42 @@ import java.net.URL;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UIAutomatorTest {
-    private AppiumDriver driver;
+    private AndroidDriver driver;
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("appium:deviceName", "Some name");
+        desiredCapabilities.setCapability("appium:deviceName", "poco");
         desiredCapabilities.setCapability("appium:appPackage", "ru.netology.testing.uiautomator");
         desiredCapabilities.setCapability("appium:appActivity", "ru.netology.testing.uiautomator.MainActivity");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
         desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
         desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
 
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+        URL remoteUrl = new URL("http://127.0.0.1:4723");
 
-        driver = new AppiumDriver(remoteUrl, desiredCapabilities);
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
     }
 
     @Test
-    public void textBeforeAndAfterTest() {
+    public void testEmptyTest() {
         MainScreen button = new MainScreen(driver);
 
-        String textBefore = button.textToBeChanged.getText();
+        String initialText = button.textToBeChanged.getText();
         button.userInput.sendKeys(" ");
         button.buttonChange.click();
-        String textAfter = button.textToBeChanged.getText();
-        Assertions.assertEquals(textBefore, textAfter);
+        String result = button.textToBeChanged.getText();
+        Assertions.assertEquals(initialText, result);
     }
 
     @Test
     public void NewActivityTest() {
         MainScreen button = new MainScreen(driver);
 
-        String newText = "Hi, Netology";
+        String newText = "Hi there";
         button.userInput.sendKeys(newText);
         button.buttonActivity.click();
         button.activityText.isDisplayed();
